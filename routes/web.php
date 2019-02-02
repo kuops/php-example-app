3c1a23b79d8b6000eeb6bc6ee24fd60e054a5e3d
kuops <opshsy@gmail.com>
2019-02-02 21:24:52 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['middleware' => ['forceSSL'],['prefix' => 'admin'], function () {
+Route::group(['middleware' => ['forceSSL']],['prefix' => 'admin'], function () {
     Voyager::routes();
 });
 
