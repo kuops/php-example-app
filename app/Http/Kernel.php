f2f56762e6fca50c8863215cbec3659ced1e3c0e
kuops <opshsy@gmail.com>
2019-02-02 19:32:39 +0800

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,7 +60,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'forceSsl' => App\Http\Middleware\ForceHttpProtocol::class,
+        'forceSsl' => App\Http\Middleware\ForceHttpsProtocol::class,
     ];
 
     /**
